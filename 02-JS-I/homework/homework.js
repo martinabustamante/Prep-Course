4a6913d5ae32e14f481ec48477abbc4932eae72b
martinabustamante <bustamantemartina256@gmail.com>
2022-09-09 20:52:16 -0300

    prueba

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -5,7 +5,7 @@ const nuevaString = null;
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
-
+// ejnfjfbjebf
 // Crea una variable booleana:
 const nuevoBool = null;
 
